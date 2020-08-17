3450cbc0a987db6706067d3135b2a6fe606035c1
Scott Daily <scottjuggles@gmail.com>
2020-08-17 12:26:41 -0400

    update time-tracker

---
+++ time-tracker/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SimpleCounter from "./Components/SimpleCounter"
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <SimpleCounter />
+    </div>
+  );
+}
+
+export default App;
---
+++ time-tracker/src/Components/SimpleCounter.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+function SimpleCounter() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <h1>
+                {count}
+            </h1>
+            <h1 onClick={() => setCount(count + 1)}>
+                +
+            </h1>
+            <h1 onClick={() => setCount(count - 1)}>
+                -
+            </h1>
+        </div>
+    )
+}
+
+export default SimpleCounter
