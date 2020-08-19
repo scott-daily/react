d5492f80a9392bbaa463841dd6cf431d4cbd602b
Scott Daily <scottjuggles@gmail.com>
2020-08-19 09:46:54 -0400

    styled buttons SimpleCounter

--- time-tracker/src/Components/SimpleCounter.js
+++ time-tracker/src/Components/SimpleCounter.js
@@ -5,18 +5,22 @@ function SimpleCounter() {
 
     return (
         <div>
-            <h1>
+            <h1 className="ml-3">
                 Count: {count}
             </h1>
-            <div class="flex mb-4">
-                <div class="w-1/2 bg-gray-400 h-12">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded" onClick={() => setCount(count + 1)}>
-                        +
+            <div>
+            <div class="inline-block text-gray-700 text-center bg-gray-200 px-4 py-2 m-2 shadow-md">
+                    <button 
+                        class="bg-red-500 hover:bg-blue-700 text-white text-xl font-bold py-1 px-4 rounded shadow-xs" 
+                        onClick={() => setCount(count - 1)}>
+                            -
                     </button>
                 </div>
-                <div class="w-1/2 bg-gray-400 h-12">
-                    <button class="bg-red-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded" onClick={() => setCount(count - 1)}>
-                        -
+                <div class="inline-block text-gray-700 text-center bg-gray-200 px-4 py-2 m-2 shadow-md">
+                    <button 
+                        class="bg-blue-500 hover:bg-blue-700 text-white text-xl font-bold py-1 px-3 rounded shadow-xs" 
+                        onClick={() => setCount(count + 1)}>
+                            +
                     </button>
                 </div>
             </div>
@@ -25,3 +29,4 @@ function SimpleCounter() {
 }
 
 export default SimpleCounter
+
