7f5fcc3610ce14ed40982e7f52505034e7968d70
Scott Daily <scottjuggles@gmail.com>
2020-08-22 09:43:01 -0400

    added loading message after submission

--- time-tracker/src/Views/Weather.js
+++ time-tracker/src/Views/Weather.js
@@ -4,10 +4,13 @@ import axios from 'axios'
 function Weather() {    
 
     const [address, setAddress] = useState("")
-    const [totalRainfall, setTotalRainfall] = useState(0)
+    const [totalRainfall, setTotalRainfall] = useState('')
+    const [loading, setLoading] = useState(false)
+    let weatherReport = <div></div>
 
     const handleAddressSubmit = (event) => {
         event.preventDefault();
+        setLoading(true)
         setAddress(event.target[0].value)
     }
 
@@ -27,43 +30,53 @@ function Weather() {
                         console.log(`Axios Call completed: ${res}`)
                         console.log("Hourly Precipitation in mm: " + res.data.data[0].prcp)
                         console.log("Hourly Temperature in celcius: " + res.data.data[0].temp)
-                        console.log(res.data.data.length)
+
                         let totalPrecip = 0
                         for (let i = 0; i < 24; i++) {
                             if (res.data.data[i].prcp != null) {
                                 totalPrecip += res.data.data[i].prcp;
                             }
                         }
+                        setLoading(false)
                         setTotalRainfall(totalPrecip)
-                        console.log(totalPrecip)
                       })
                 }, (error) => {
                     console.log(error)
                 })}
             }, [address])
+
+            if (loading) {
+                weatherReport = <p className="ml-10">...loading</p>
+            }
+            else {
+                weatherReport =
+                <div className="ml-5">
+                    Total Rainfall in mm on 2020-05-19: <b>{totalRainfall}</b>
+                </div> 
+            }
         
 
     return (
         <div>
-            <h1 className="mb-3">Historical Weather Info </h1>
+            <h1 className="mb-4 text-lg ml-8">Historical Weather Info </h1>
                 <form onSubmit={handleAddressSubmit}
                     className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <label className="mr-2 pl-1 block text-gray-700 text-sm font-bold mb-2">Enter an address</label>
+                    <label className="mr-2 pl-1 block text-gray-700 text-md font-bold mb-2">Enter a location</label>
                         <input 
                             type = "text" 
                             name="address"
-                            placeholder={"Enter a location"}
-                            className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                            placeholder={"Location"}
+                            className="shadow appearance-none border rounded py-2 px-3 text-md text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                         />
                     <input type="submit" 
                         value="Submit" 
                         className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
                         >
                     </input>
-                    <div className="block ml-1 mt-5">
-                        Total Rainfall in mm: {totalRainfall}
-                    </div>
                 </form>
+                <div className="block mt-12 text-xl">
+                        {weatherReport}
+                </div>
         </div>
     )
 }
