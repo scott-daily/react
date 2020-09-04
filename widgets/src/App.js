df53a9f633b173d9799d413b0e9ae8466a5369c8
Scott Daily <scottjuggles@gmail.com>
2020-09-04 15:47:02 -0400

    started Dropdown component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from './components/Accordion';
 import Dropdown from './components/Dropdown';
 import Search from './components/Search';
@@ -18,11 +18,34 @@ const items = [
     },
 ];
 
+const options = [
+    {
+        label: 'Red',
+        value: 'red',
+    },
+    {
+        label: 'Green',
+        value: 'green',
+    },
+    {
+        label: 'Blue',
+        value: 'blue'
+    }
+]
+
 
 export default () => {
+
+    const [selected, setSelected] = useState(options[0])
+
     return  (
         <div>
-           <Dropdown />
+           <Dropdown 
+                selected={selected}
+                dropdownLabel='Select a Color' 
+                options={options} 
+                onSelectedChange={setSelected}
+            />
         </div>
     )
 };
--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Dropdown = () => {
+const Dropdown = (props) => {
+    const [open, setOpen] = useState(false);
+
+        const renderedOptions = props.options.map((option) => {
+            if (option.value === props.selected.value) {
+                return null;
+            }
+            return (
+                <div 
+                    key={option.value} 
+                    className="item"
+                    onClick={() => props.onSelectedChange(option)}
+                >
+                    {option.label}
+                </div>
+            )
+        });
+
     return (
-        <div>Dropdown</div>
-    )
-}
+        <div className="ui form">
+            <div className="field">
+                <label className="label">{props.dropdownLabel}</label>
+                    <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
+                        <i className="dropdown icon"></i>
+                        <div className="text">{props.selected.label}</div>
+                            <div className={`menu ${open ? 'visible transition' : ''}`}>{renderedOptions}</div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Dropdown;
