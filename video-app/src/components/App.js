3885f52b77ab954be5d507b6559291aa69c197fa
Scott Daily <scottjuggles@gmail.com>
2020-09-10 11:22:35 -0400

    created custom useVideos hook

--- video-app/src/components/App.js
+++ video-app/src/components/App.js
@@ -2,59 +2,46 @@ import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import VideoDetail from './VideoDetail';
 import VideoList from './VideoList';
-import youtube from '../apis/youtube'
+import useVideos from '../hooks/useVideos';
 import { render } from '@testing-library/react';
 
 
 const App = () => {
 
-    const [videos, setVideos] = useState([]);
     const [selectedVideo, setSelectedVideo] = useState(null);
+    const [videos, search] = useVideos('c++')
 
     useEffect(() => {
-        onSearchSubmit('react js');
-    }, []);
+        setSelectedVideo(videos[0]);
+    }, [videos]);
 
-    const onSearchSubmit = async (term) => {
-        const response = await youtube.get('/search', {
-             params: { q: term }
-         });
 
-         setVideos(response.data.items) 
-         setSelectedVideo(response.data.items[0]);
-    };
-
-    const onVideoSelect = (video) => {
-        setSelectedVideo(video);
-    } 
-
-     render()
-        if (window.matchMedia("(max-width: 700px)").matches) {
-            return (
-                <div className="ui container">
-                    <SearchBar onFormSubmit={onSearchSubmit}/>
-                    <VideoDetail video={selectedVideo} />
-                    <VideoList onVideoSelect={onVideoSelect} videos={videos} />
-                </div>
-            )
-        } else {
-            return (
-                <div className="ui container">
-                    <SearchBar onFormSubmit={onSearchSubmit}/>
-                    <div className="ui grid">
-                        <div className="ui row">
-                            <div className="eleven wide column">
-                                <VideoDetail video={selectedVideo} />
-                            </div>
-                            <div className="five wide column">
-                                <VideoList onVideoSelect={onVideoSelect} videos={videos} />
-                            </div>
+    if (window.matchMedia("(max-width: 700px)").matches) {
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={search}/>
+                <VideoDetail video={selectedVideo} />
+                <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
+            </div>
+        )
+    } else {
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={search}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
                         </div>
                     </div>
                 </div>
-            )
-        }
+            </div>
+        )
     }
+}
 
 export default App;
 
